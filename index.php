4a02605777ee6fd7f0d29cd71f130801bbebf859
Ashton Knox <aknox3@mail.greenriver.edu>
2018-01-12 17:45:57 -0500

    Final commit for reals

--- index.php
+++ index.php
@@ -3,8 +3,22 @@
 
     $f3 = Base::instance();
 
+    $f3->set('DEBUG',3);
+
     $f3->route('GET /', function() {
         echo '<h1>ROUTING DEMO</h1>';
+    });
+
+    $f3->route('GET /page1', function() {
+    echo '<h1>THIS IS PAGE ONE!!1!!11!</h1>';
+    });
+
+    $f3->route('GET /page2', function() {
+    echo '<h1>THIS IS PAGE TWO</h1>';
+    });
+
+    $f3->route('GET /page1/subpage-a', function() {
+        echo '<h1>THIS IS PAGE ONE SUB A!!</h1>';
     });
 
     $f3->run();
