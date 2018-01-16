741c812110bcd11ee3100bb0742ff2866f2a125e
Ashton Knox <aknox3@mail.greenriver.edu>
2018-01-16 16:50:47 -0500

    Added hi.html and hello.html

--- index.php
+++ index.php
@@ -33,8 +33,23 @@
 
     $f3->route('GET /hello/@name',
         function($f3, $params) {
-        $name = $params['name'];
-        echo "<h1>Hello, $name</h1>";
+        //$name = $params['name'];
+        //echo "<h1>Hello, $name</h1>";
+
+        $f3->set('name', $params['name']);
+        $template = new Template();
+        echo $template->render('views/hello.html');
+        });
+
+    $f3->route('GET /hi/@first/@last',
+        function($f3, $params) {
+
+            $f3->set('first', $params['first']);
+            $f3->set('last', $params['last']);
+            $f3->set('message', 'Hi');
+
+            $template = new Template();
+            echo $template->render('views/hi.html');
         });
 
     $f3->run();
